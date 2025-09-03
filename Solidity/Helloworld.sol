c9161081ed1fdf8b307e2c170ed7bf8ae79c8798
Rabindra Mishra <sambhu311275@gmail.com>
2025-09-03 17:19:57 +0530

    Basics of solidity

---
+++ Solidity/Helloworld.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.13;
+
+contract HelloWorld{
+    string public greeting="Hello,world";
+    uint public myNumber=30;
+}
+//Ethereum has evm thus we send byte code to evm. Also byte code is quite fast.
