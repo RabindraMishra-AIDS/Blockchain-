8ab36a9541ae9e18ebd79d5297d5e8c5372b54fe
Rabindra Mishra <sambhu311275@gmail.com>
2025-09-04 16:30:35 +0530

    payable function concept

---
+++ Solidity/Payable Functions.sol
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+contract FuncPay {
+    function getTip() public  payable {
+        //payable keyword describes that function can be used to accept money
+    } 
+}
