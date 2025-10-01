5df15391e4442f778d870d7149c0c4ffcfbd52a1
RabindraMishra-AIDS <sambhu311275@gmail.com>
2025-10-01 19:14:25 +0530

    Basic Datatypes in RUST

---
+++ Rust/Datatypes03.rs
@@ -0,0 +1,13 @@
+//================ Datatypes in RUST ==================
+
+fn main(){
+    let age:u8=30;  //unsigned int
+
+    let percentile:f32=99.678484;  //float 32
+
+    let is_logout:bool=true;
+
+    let letter:char='R'; //use single quotes
+
+    println!("age is:{}, Percentile scored = {} and user is_logout={},Rust starts with:{}",age,percentile,is_logout,letter);
+}
