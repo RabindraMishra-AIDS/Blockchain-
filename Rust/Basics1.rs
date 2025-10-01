ac12de521f1a289400f507ffc88df47db2bee7ad
RabindraMishra-AIDS <sambhu311275@gmail.com>
2025-10-01 17:41:11 +0530

    Hello world in RUST

---
+++ Rust/Basics1.rs
@@ -0,0 +1,7 @@
+fn main(){
+    println!("Hello World, I am Rust");
+    let a=22;
+    println!("{}",a);
+}
+// Rust works on the philosophy of immutability.
+//Note in rust println is marco(uses exclamation mark).
